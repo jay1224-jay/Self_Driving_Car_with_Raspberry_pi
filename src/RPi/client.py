27ed4ed525ccd8498d05a2d585dca3a3c573626d
jay1224-jay <wuj20061224@gmail.com>
2023-12-24 16:56:13 +0800

    show received message from server

--- src/RPi/client.py
+++ src/RPi/client.py
@@ -6,20 +6,17 @@ port = 5000  # socket server port number
 client_socket = socket.socket()  # instantiate
 client_socket.connect((host, port))  # connect to the server
 
-cap = cv2.VideoCapture(-1)  # use 0 if not using Raspberry Pi camera slot
+cap = cv2.VideoCapture(0)  # use 0 if not using Raspberry Pi camera slot
 
 preMessage = ""
 while True:
 
     ret,photo = cap.read()
-    # print(photo[0], photo[1])
 
-    cv2.imshow('streaming',photo)
-    ret,buffer = cv2.imencode(".jpg",photo,[int(cv2.IMWRITE_JPEG_QUALITY),20])
+
+    # cv2.imshow('streaming',photo)
+    ret,buffer = cv2.imencode(".jpg",photo,[int(cv2.IMWRITE_JPEG_QUALITY),30])
     x_as_bytes = pickle.dumps(buffer)
-    print("send message to server")
-    # s.sendto((x_as_bytes),(server_ip,server_port))
-    print(x_as_bytes, len(x_as_bytes))
     client_socket.send(x_as_bytes)
     client_socket.send(b"stop")
     
@@ -32,13 +29,15 @@ while True:
             0 stands for left
             1 stands for right
     """
-    if messageFromServer == "0":
+    if messageFromServer == "0" and messageFromServer != preMessage:
+        preMessage = "0"
         print("Turn left")
-    elif messageFromServer == "1":
+    elif messageFromServer == "1" and messageFromServer != preMessage:
+        preMessage = "1"
         print("Turn right")
-    elif messageFromServer == "3":
+    elif messageFromServer == "start" and messageFromServer != preMessage:
+        preMessage = "start"
         print("Start objtracking")
-    print(messageFromServer)
 
     
     if cv2.waitKey(10)==ord('q'):
